970a814c4704a8b9913c329f8485a3ca7e0cbda7
komodgn <a01075251881@gmail.com>
2025-12-16 13:43:18 +0900

    feat: 메인 탭 바에서 사용할 기본 스크린 등록 (#12)

--- feature/screens/build.gradle.kts
+++ feature/screens/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     alias(libs.plugins.metasearch.android.library)
+    alias(libs.plugins.metasearch.android.library.compose)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
     namespace = "com.example.metasearch.feature.screens"
+}
+
+dependencies {
+    implementation(libs.circuit.foundation)
 }
