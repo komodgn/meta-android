45f91b2fc90d0482e6e27b9e58263e90f2435704
komodgn <a01075251881@gmail.com>
2025-12-18 17:52:05 +0900

    fix: optimize imports

--- feature/person/src/main/java/com/example/metasearch/feature/person/PersonUi.kt
+++ feature/person/src/main/java/com/example/metasearch/feature/person/PersonUi.kt
@@ -2,7 +2,6 @@ package com.example.metasearch.feature.person
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
