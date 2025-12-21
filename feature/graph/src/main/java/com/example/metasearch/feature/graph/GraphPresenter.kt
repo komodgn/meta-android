c41dcc0c0f6c75ad39453168cbcd7c8b596c2e12
komodgn <a01075251881@gmail.com>
2025-12-22 00:58:27 +0900

    feat: 전체 그래프 화면 추가
    - 갤러리 속 모든 데이터를 포함한 그래프를 출력하는 웹뷰 포함
    
    TODO:
    - 공통 로직 분리

--- feature/graph/src/main/java/com/example/metasearch/feature/graph/GraphPresenter.kt
+++ feature/graph/src/main/java/com/example/metasearch/feature/graph/GraphPresenter.kt
@@ -1,6 +1,15 @@
 package com.example.metasearch.feature.graph
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import com.example.metasearch.core.data.api.repository.GraphRepository
+import com.example.metasearch.feature.screens.GraphDetailScreen
 import com.example.metasearch.feature.screens.GraphScreen
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.Navigator
@@ -9,29 +18,66 @@ import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 import dagger.hilt.android.components.ActivityRetainedComponent
+import kotlinx.coroutines.launch
 
 class GraphPresenter @AssistedInject constructor(
     @Assisted private val navigator: Navigator,
+    private val graphRepository: GraphRepository,
 ) : Presenter<GraphUiState> {
 
     @Composable
     override fun present(): GraphUiState {
+        var webViewUrl by remember { mutableStateOf("") }
+        var selectedImages by remember { mutableStateOf(listOf<String>()) }
+        var errorMessage by remember { mutableStateOf("") }
+        val coroutineScope = rememberCoroutineScope()
+        val imageNotFoundMessage = stringResource(R.string.graph_screen_image_not_found_error)
+        val MAX_IMAGES = 10
+
+        LaunchedEffect(Unit) {
+//            webViewUrl = "https://www.google.com"
+            webViewUrl = graphRepository.getFullGraphWebViewUrl()
+        }
+
         fun handleEvent(event: GraphUiEvent) {
             when (event) {
-                is GraphUiEvent.OnTabClick -> {
-                    navigator.resetRoot(event.screen)
+                is GraphUiEvent.OnPhotoSelected -> {
+                    coroutineScope.launch {
+                        val uri = graphRepository.findMatchedUri(event.photoName)
+                        if (uri != null) {
+                            val uriString = uri.toString()
+                            if (!selectedImages.contains(uriString)) {
+                                selectedImages = (listOf(uriString) + selectedImages).take(MAX_IMAGES)
+                            }
+                        } else {
+                            errorMessage = imageNotFoundMessage
+                        }
+                    }
                 }
+
+                is GraphUiEvent.OnImageClick -> {
+                    navigator.goTo(GraphDetailScreen(event.uriString))
+                }
+
+                GraphUiEvent.OnErrorDialogDismiss -> errorMessage = ""
+
+                is GraphUiEvent.OnTabClick -> navigator.resetRoot(event.screen)
             }
         }
 
         return GraphUiState(
+            webViewUrl = webViewUrl,
+            selectedImages = selectedImages,
+            errorMessage = errorMessage,
             eventSink = ::handleEvent,
         )
     }
 
     @CircuitInject(GraphScreen::class, ActivityRetainedComponent::class)
     @AssistedFactory
-    fun interface Factory {
-        fun create(navigator: Navigator): GraphPresenter
+    interface Factory {
+        fun create(
+            navigator: Navigator,
+        ): GraphPresenter
     }
 }
--- feature/graph/src/main/java/com/example/metasearch/feature/graph/GraphUiState.kt
+++ feature/graph/src/main/java/com/example/metasearch/feature/graph/GraphUiState.kt
@@ -5,10 +5,30 @@ import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
 
 data class GraphUiState(
+    val webViewUrl: String = "",
+    val selectedImages: List<String> = emptyList(),
+    val isLoading: Boolean = false,
+    val errorMessage: String = "",
     val eventSink: (GraphUiEvent) -> Unit,
 ) : CircuitUiState
 
 sealed interface GraphUiEvent : CircuitUiEvent {
+    data class OnPhotoSelected(
+        val photoName: String,
+    ) : GraphUiEvent
+
+    /**
+     * 사진 상세 화면으로 이동
+     */
+    data class OnImageClick(
+        val uriString: String,
+    ) : GraphUiEvent
+
+    data object OnErrorDialogDismiss : GraphUiEvent
+
+    /**
+     * 하단 네비 탭 클릭
+     */
     data class OnTabClick(
         val screen: Screen,
     ) : GraphUiEvent
