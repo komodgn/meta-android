e16a79aa5480b2574cc40555e5c636e333a434fb
komodgn <a01075251881@gmail.com>
2025-12-23 00:46:37 +0900

    feat(home): 이미지 분석 기능 추가 (#22)
    - 홈 헤더에 이미지 분석 버튼 추가
    - 분석 상태에 따라 아이콘 변경

--- feature/home/src/main/java/com/example/metasearch/feature/home/component/HomeHeader.kt
+++ feature/home/src/main/java/com/example/metasearch/feature/home/component/HomeHeader.kt
@@ -7,20 +7,29 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.metasearch.core.designsystem.annotation.ComponentPreview
 import com.example.metasearch.core.designsystem.theme.LightGrey
 import com.example.metasearch.core.designsystem.theme.MetaSearchTheme
+import com.example.metasearch.core.designsystem.theme.Neutral500
 import com.example.metasearch.core.designsystem.theme.Neutral800
 import com.example.metasearch.feature.home.R
 
 @Composable
 fun HomeHeader(
     modifier: Modifier = Modifier,
+    onUploadClick: () -> Unit,
+    isAnalyzing: Boolean,
 ) {
     Column {
         Row(
@@ -33,6 +42,23 @@ fun HomeHeader(
                 style = MetaSearchTheme.typography.headlineSmall,
                 color = Neutral800,
             )
+            Spacer(
+                modifier = Modifier.weight(1f),
+            )
+            if (isAnalyzing) {
+                CircularProgressIndicator(modifier = Modifier.size(20.dp))
+            } else {
+                IconButton(
+                    onClick = onUploadClick,
+                ) {
+                    Icon(
+                        modifier = Modifier.size(24.dp),
+                        painter = painterResource(R.drawable.ic_upload),
+                        tint = Neutral500,
+                        contentDescription = "Upload Icon",
+                    )
+                }
+            }
         }
         Spacer(
             modifier = modifier.fillMaxWidth()
@@ -46,6 +72,9 @@ fun HomeHeader(
 @Composable
 fun HomeHeaderPreview() {
     MetaSearchTheme {
-        HomeHeader()
+        HomeHeader(
+            isAnalyzing = true,
+            onUploadClick = {},
+        )
     }
 }
