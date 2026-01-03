5c5973ed6c96e8f5ca8702e52a5162f2858de36c
komodgn <a01075251881@gmail.com>
2026-01-03 18:07:37 +0900

    delete: 사용하지 않는 코드 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,22 +14,7 @@ ksp {
     arg("circuit.codegen.mode", "hilt")
 }
 
-// -------------리팩토링 중
 dependencies {
-
-//    implementation("androidx.work:work-runtime:2.9.0")
-
-//    // CircleImageView dependency
-//    implementation("de.hdodenhof:circleimageview:3.1.0")
-//    // PhotoView dependency
-//    implementation("com.github.chrisbanes:PhotoView:2.3.0")
-//    // SpinKit dependency
-//    implementation("com.github.ybq:Android-SpinKit:1.4.0")
-//    // CardView dependency
-//    implementation("androidx.cardview:cardview:1.0.0")
-//    // ColorPickerDialog dependency
-//    implementation("me.jfenn.ColorPickerDialog:base:0.2.2")
-
     implementation(projects.core.common)
     implementation(projects.core.data.api)
     implementation(projects.core.data.impl)
