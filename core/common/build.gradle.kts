0cf077171a3ce22f2e4f556a49bb7c777a2d8a7c
komodgn <a01075251881@gmail.com>
2026-01-08 17:19:22 +0900

    chore(common): Retrofit 의존성 추가 (#68)

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
     alias(libs.plugins.metasearch.android.library)
     alias(libs.plugins.metasearch.android.library.compose)
+    alias(libs.plugins.metasearch.android.retrofit)
 }
 
 android {
     namespace = "com.example.metasearch.core.common"
+}
+
+dependencies {
+    implementation(projects.core.network)
 }
