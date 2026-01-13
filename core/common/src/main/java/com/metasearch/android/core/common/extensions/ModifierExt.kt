324996c25c743af935dc3ec046c9596ecf6aec20
komodgn <a01075251881@gmail.com>
2026-01-13 15:36:15 +0900

    feat: 프리뷰용 이미지 플레이스홀더 확장 함수 추가 (#81)
    - 이미지 컴포넌트를 프리뷰에서 확인할 수 있도록 로컬 모드일 때만 배경색을 반환하는 확장 함수 구현

--- core/common/src/main/java/com/metasearch/android/core/common/extensions/ModifierExt.kt
+++ core/common/src/main/java/com/metasearch/android/core/common/extensions/ModifierExt.kt
@@ -1,7 +1,23 @@
 package com.metasearch.android.core.common.extensions
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalInspectionMode
 
 fun Modifier.clickableIfNotNull(onClick: (() -> Unit)?): Modifier =
     if (onClick != null) this.clickable(onClick = onClick) else this
+
+@Composable
+fun Modifier.previewPlaceholder(
+    color: Color = LightGray,
+    shape: Shape? = null
+): Modifier = if (LocalInspectionMode.current) {
+    if (shape != null) background(color, shape) else background(color)
+} else {
+    this
+}
