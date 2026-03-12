0c226015d47fba392222e0e7587b56ae1f280be9
komodgn <a01075251881@gmail.com>
2026-03-12 12:20:32 +0900

    style: ktlintFormat
    - Optimize imports

--- feature/graph/src/main/java/com/metasearch/android/feature/graph/GraphPresenter.kt
+++ feature/graph/src/main/java/com/metasearch/android/feature/graph/GraphPresenter.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import com.metasearch.android.core.common.utils.UiText
