9647192330475f9ab0c5feb224df776bcc54d1e4
komodgn <a01075251881@gmail.com>
2026-04-12 19:00:52 +0900

    test: Implement Unit Tests for PersonWebService

---
+++ data/remote/src/test/java/com/metasearch/android/data/remote/PersonWebServiceTest.kt
@@ -0,0 +1,45 @@
+package com.metasearch.android.data.remote
+
+import com.google.common.truth.Truth.assertThat
+import com.metasearch.android.data.remote.person.request.PersonFrequencyRequest
+import com.metasearch.android.data.remote.person.service.PersonWebService
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+
+class PersonWebServiceTest : ApiAbstract<PersonWebService>() {
+
+    private lateinit var service: PersonWebService
+
+    @Before
+    fun initService() {
+        super.createMockServer()
+        service = createService(PersonWebService::class.java)
+    }
+
+    @Test
+    fun `getPersonFrequency parses response correctly`() = runTest {
+        // given
+        enqueueResponse("/PersonFrequencyResponse.json")
+        val mockRequest = PersonFrequencyRequest(
+            dbName = "dbabf1e5c83b8b485da6ba3b94ebf78dgs",
+            personNames = listOf("홍철", "지수"),
+        )
+
+        // when
+        val response = service.getPersonFrequency(mockRequest)
+
+        // then
+        assertThat(response.frequencies).hasSize(2)
+
+        with(response.frequencies[0]) {
+            assertThat(personName).isEqualTo("홍철")
+            assertThat(frequency).isEqualTo(15)
+        }
+
+        with(response.frequencies[1]) {
+            assertThat(personName).isEqualTo("지수")
+            assertThat(frequency).isEqualTo(8)
+        }
+    }
+}
