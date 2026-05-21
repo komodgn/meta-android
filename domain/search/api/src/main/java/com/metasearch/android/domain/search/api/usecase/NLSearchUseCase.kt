c13c66068e96a1ed7d67d333d712708d89db8950
komodgn <a01075251881@gmail.com>
2026-05-22 00:16:16 +0900

    feat: Add toggle for local/remote natural language search
    - On-device Keyword Extraction for Hybrid Search.

--- domain/search/api/src/main/java/com/metasearch/android/domain/search/api/usecase/NLSearchUseCase.kt
+++ domain/search/api/src/main/java/com/metasearch/android/domain/search/api/usecase/NLSearchUseCase.kt
@@ -3,5 +3,5 @@ package com.metasearch.android.domain.search.api.usecase
 import com.metasearch.android.data.domain.NLSearchResult
 
 interface NLSearchUseCase {
-    suspend operator fun invoke(query: String): Result<NLSearchResult>
+    suspend operator fun invoke(query: String, isLocal: Boolean): Result<NLSearchResult>
 }
--- feature/search/src/main/java/com/metasearch/android/feature/search/nls/NLSearchPresenter.kt
+++ feature/search/src/main/java/com/metasearch/android/feature/search/nls/NLSearchPresenter.kt
@@ -1,16 +1,21 @@
 package com.metasearch.android.feature.search.nls
 
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import com.metasearch.android.core.common.utils.UiText
+import com.metasearch.android.core.common.utils.UiText.StringResource
 import com.metasearch.android.core.common.utils.handleException
+import com.metasearch.android.domain.search.api.repository.SearchRepository
 import com.metasearch.android.domain.search.api.usecase.NLSearchUseCase
 import com.metasearch.android.feature.screens.NLSearchScreen
 import com.metasearch.android.feature.screens.PhotoDetailScreen
 import com.metasearch.android.feature.search.R
+import com.metasearch.android.feature.search.nls.NLSearchSideEffect.ShowToast
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.retained.rememberRetained
 import com.slack.circuit.runtime.Navigator
@@ -24,10 +29,13 @@ import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
 import kotlinx.coroutines.launch
 
+private const val TAG = "NLSearchPresenter"
+
 @AssistedInject
 class NLSearchPresenter(
     @Assisted private val navigator: Navigator,
     private val nlSearchUseCase: NLSearchUseCase,
+    private val searchRepository: SearchRepository,
 ) : Presenter<NLSearchUiState> {
 
     @CircuitInject(NLSearchScreen::class, AppScope::class)
@@ -43,6 +51,13 @@ class NLSearchPresenter(
         var sideEffect by rememberRetained { mutableStateOf<NLSearchSideEffect?>(null) }
         var inputString by rememberRetained { mutableStateOf("") }
         var resultImages by rememberRetained { mutableStateOf<ImmutableList<String>>(persistentListOf()) }
+
+        val isLocalEngineReady by remember {
+            derivedStateOf { searchRepository.isLocalModelAvailable() }
+        }
+        var isLocalSearchEnabled by remember { mutableStateOf(false) }
+        Log.d(TAG, isLocalSearchEnabled.toString())
+        val effectiveLocalSearchEnabled = isLocalEngineReady && isLocalSearchEnabled
 
         fun handleEvent(event: NLSearchUiEvent) {
             when (event) {
@@ -61,18 +76,18 @@ class NLSearchPresenter(
                     isLoading = true
 
                     scope.launch {
-                        nlSearchUseCase(inputString)
+                        nlSearchUseCase(inputString, isLocalSearchEnabled)
                             .onSuccess { result ->
                                 if (result.matchedUris.isEmpty()) {
                                     resultImages = persistentListOf()
-                                    sideEffect = NLSearchSideEffect.ShowToast(
-                                        message = UiText.StringResource(R.string.search_screen_empty_result_message),
+                                    sideEffect = ShowToast(
+                                        message = StringResource(R.string.search_screen_empty_result_message),
                                     )
                                 } else {
                                     resultImages = result.matchedUris.toPersistentList()
                                 }
                             }.onFailure { exception ->
-                                handleException(exception, onError = { sideEffect = NLSearchSideEffect.ShowToast(it) })
+                                handleException(exception, onError = { sideEffect = ShowToast(it) })
                             }
                         isLoading = false
                     }
@@ -85,6 +100,12 @@ class NLSearchPresenter(
                 is NLSearchUiEvent.OnTabClick -> {
                     navigator.resetRoot(event.screen)
                 }
+
+                is NLSearchUiEvent.OnToggleLocalSearch -> {
+                    if (isLocalEngineReady) {
+                        isLocalSearchEnabled = event.isEnabled
+                    }
+                }
             }
         }
 
@@ -92,6 +113,8 @@ class NLSearchPresenter(
             isLoading = isLoading,
             inputString = inputString,
             resultImages = resultImages,
+            isLocalEngineReady = isLocalEngineReady,
+            isLocalSearchEnabled = effectiveLocalSearchEnabled,
             sideEffect = sideEffect,
             eventSink = ::handleEvent,
         )
